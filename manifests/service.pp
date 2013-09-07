a3922d237572de2dd2574c9d658391ae88d831ef
Michael Arnold <github@razorsedge.org>
2013-09-06 23:10:23 -0700

    Advanced configuration of ipmievd service.

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,12 +5,30 @@
 # === Authors
 #
 # Joshua Hoblitt <jhoblitt@cpan.org>
+# Mike Arnold <mike@razorsedge.org>
 #
 # === Copyright
 #
 # Copyright (C) 2013 Joshua Hoblitt
+# Copyright (C) 2013 Mike Arnold, unless otherwise noted.
 #
-class ipmi::service {
+class ipmi::service (
+  $start_ipmievd = false,
+) {
+  case $start_ipmievd {
+    /(true)/: {
+      $ipmievd_ensure = 'running'
+      $ipmievd_enable = true
+    }
+    /(false)/: {
+      $ipmievd_ensure = 'stopped'
+      $ipmievd_enable = false
+    }
+    default: {
+      fail('start_ipmievd parameter must be true or false')
+    }
+  }
+
   service{ 'ipmi':
     ensure      => running,
     hasstatus   => true,
@@ -19,9 +37,9 @@ class ipmi::service {
   }
 
   service{ 'ipmievd':
-    ensure      => running,
+    ensure      => $ipmievd_ensure,
     hasrestart  => true,
     hasstatus   => true,
-    enable      => true,
+    enable      => $ipmievd_enable,
   }
 }
--- spec/classes/ipmi_service_spec.rb
+++ spec/classes/ipmi_service_spec.rb
@@ -13,11 +13,26 @@ describe 'ipmi::service', :type => :class do
 
   it do
     should contain_service('ipmievd').with({
-      :ensure => 'running',
+      :ensure => 'stopped',
       :hasstatus => 'true',
       :hasrestart => 'true',
-      :enable => 'true',
+      :enable => 'false',
     })
   end
 
+  describe 'with start_ipmievd => true' do
+    let :params do
+      {
+        :start_ipmievd => 'true',
+      }
+    end
+    it do
+      should contain_service('ipmievd').with({
+        :ensure => 'running',
+        :hasstatus => 'true',
+        :hasrestart => 'true',
+        :enable => 'true',
+      })
+    end
+  end
 end
