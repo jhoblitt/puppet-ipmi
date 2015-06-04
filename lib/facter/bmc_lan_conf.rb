a650d673b2e45253256b90b7648b2c5acb4f3a94
Fabien Wernli <wernli@in2p3.fr>
2015-06-04 15:42:37 +0200

    Implement bmc_lan_conf structured fact

---
+++ lib/facter/bmc_lan_conf.rb
@@ -0,0 +1,31 @@
+# Fact: bmc_lan_conf
+#
+# Purpose: get IP of BMC
+#
+# Resolution:
+#   Uses freeipmi query tool
+#   bmc-config --checkout -e Lan_Conf:IP_Address
+#   Section Lan_Conf
+#           ## Give valid IP address
+#           IP_Address                                    192.168.168.17
+#   EndSection
+#
+# Caveats:
+#   Needs freeipmi. If you find a better way using OpenIPMI
+# please tell me
+#
+# Notes:
+#   None
+Facter.add(:bmc_lan_conf, :timeout => 5) do
+  confine :kernel => "Linux"
+  confine :is_virtual => false
+  setcode do
+    if Facter::Util::Resolution.which('bmc-config')
+      output = Facter::Util::Resolution.exec("bmc-config --checkout --section Lan_Conf --disable-auto-probe").lines.find_all { |l| l =~ /^\s+[^#]/}
+      if ! output.empty?
+        Hash[output.map { |e| e.split }]
+      end
+    end
+  end
+end
+
