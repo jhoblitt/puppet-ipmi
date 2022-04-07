39a0daf28804ec86ebad375a8d4e9917b519de9a
Joshua Hoblitt <josh@hoblitt.com>
2022-04-07 15:46:37 -0700

    add Debian support to acceptance tests

--- spec/acceptance/ipmi_spec.rb
+++ spec/acceptance/ipmi_spec.rb
@@ -3,12 +3,17 @@
 require 'spec_helper_acceptance'
 
 describe 'ipmi class' do
-  package_name = nil
-  case fact('os.release.major')
-  when '5'
-    package_name = %w[OpenIPMI OpenIPMI-tools]
-  when '6', '7'
-    package_name = %w[OpenIPMI ipmitool]
+  case fact('os.family')
+  when 'RedHat'
+    packages = %w[
+      OpenIPMI
+      ipmitool
+    ]
+  when 'Debian'
+    packages = %w[
+      openipmi
+      ipmitool
+    ]
   end
 
   describe 'running puppet code' do
@@ -17,7 +22,7 @@ describe 'ipmi class' do
         class { 'epel': } -> Class['ipmi']
       }
 
-      include ::ipmi
+      include ipmi
     PP
 
     it 'applies the manifest' do
@@ -25,7 +30,7 @@ describe 'ipmi class' do
       apply_manifest(pp, catch_failures: false)
     end
 
-    package_name.each do |pkg|
+    packages.each do |pkg|
       describe package(pkg) do
         it { is_expected.to be_installed }
       end
