e4d9bd6662249494b1872d913e0721c27fa31564
Pat Riehecky <riehecky@fnal.gov>
2022-04-12 09:24:57 -0500

    Added control for interface failover on IPMI

--- manifests/network.pp
+++ manifests/network.pp
@@ -10,6 +10,11 @@
 #   Controls the if IP will be from DHCP or Static.
 # @param lan_channel
 #   Controls the lan channel of the IPMI network to be configured.
+# @param interface_type
+#   Controls the IPMI failover settings
+#   NOTE: each BMC vendor has their own method for doing this.
+#         If your vendor is not listed here, it will do nothing.
+#   Known vendors: Supermicro, Dell
 #
 define ipmi::network (
   Stdlib::IP::Address $ip      = '0.0.0.0',
@@ -17,8 +22,68 @@ define ipmi::network (
   Stdlib::IP::Address $gateway = '0.0.0.0',
   Enum['dhcp', 'static'] $type = 'dhcp',
   Integer $lan_channel         = 1,
+  Optional[Enum['dedicated', 'shared', 'failover']] $interface_type = undef,
 ) {
   require ipmi
+
+  if defined($interface_type) {
+    if 'Manufacturer Name' in $facts['ipmitool_mc_info'] {
+      case $facts['ipmitool_mc_info']['Manufacturer Name'] {
+        'Supermicro': {
+          $interface_type_raw_command_code = '0x30 0x70 0x0c'
+          case $interface_type {
+            'dedicated': {
+              $interface_code = 0
+            }
+            'shared': {
+              $interface_code = 1
+            }
+            'failover': {
+              $interface_code = 2
+            }
+            default: {
+              $interface_code = undef
+            }
+          }
+          # lint:ignore:140chars
+          exec { "ipmi_set_interface_type_${lan_channel}":
+            command => "/usr/bin/ipmitool raw ${interface_type_raw_command_code} 1 ${interface_code}",
+            onlyif  => "/usr/bin/test $(/usr/bin/ipmitool raw ${interface_type_raw_command_code} 0) -ne ${interface_code}",
+          }
+          # lint:endignore
+        }
+        'Dell': {
+          $interface_type_raw_set_code = '0x30 0x24'
+          $interface_type_raw_check_code = '0x30 0x25'
+          case $interface_type {
+            'dedicated': {
+              $interface_code = 2
+            }
+            'shared': {
+              $interface_code = 0
+            }
+            'failover': {
+              $interface_code = 1
+            }
+            default: {
+              $interface_code = undef
+            }
+          }
+          # lint:ignore:140chars
+          exec { "ipmi_set_interface_type_${lan_channel}":
+            command => "/usr/bin/ipmitool raw ${interface_type_raw_set_code} ${interface_code}",
+            onlyif  => "/usr/bin/test $(/usr/bin/ipmitool raw ${interface_type_raw_check_code}) -ne ${interface_code}",
+          }
+          # lint:endignore
+        }
+        default: {
+          # lint:ignore:140chars
+          warning("${facts['ipmitool_mc_info']['Manufacturer Name']} does not have interface type behavior defined in this module.  Can you submit it?")
+          # lint:endignore
+        }
+      }
+    }
+  }
 
   if $type == 'dhcp' {
     exec { "ipmi_set_dhcp_${lan_channel}":
